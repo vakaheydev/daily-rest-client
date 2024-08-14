9ea2f26965d8c4aaeb39fa9d2a957c25814f0d0c
vakaheydev <116391267+vakaheydev@users.noreply.github.com>
2024-08-14 13:54:20 +0300

    Added getters and setters to UserDTO

--- src/main/java/com/vaka/daily_client/model/dto/UserDTO.java
+++ src/main/java/com/vaka/daily_client/model/dto/UserDTO.java
@@ -2,6 +2,7 @@ package com.vaka.daily_client.model.dto;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 
 public class UserDTO {
     @NotEmpty
@@ -22,4 +23,44 @@ public class UserDTO {
 
     @Size(max = 100)
     private String patronymic;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
 }
