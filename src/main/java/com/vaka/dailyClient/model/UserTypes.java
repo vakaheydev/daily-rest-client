8a49d2fce808bb0f15d97e63c9bb00f8eec00d06
vakaheydev <116391267+vakaheydev@users.noreply.github.com>
2024-10-07 17:16:59 +0300

    Added type to UserTypes enum

--- src/main/java/com/vaka/dailyClient/model/UserTypes.java
+++ src/main/java/com/vaka/dailyClient/model/UserTypes.java
@@ -1,2 +1,18 @@
-package com.vaka.dailyClient.model;public enum UserTypes {
+package com.vaka.dailyClient.model;
+
+public enum UserTypes {
+    USER(new UserType(1, "user")),
+    VIP(new UserType(2, "vip")),
+    ADMIN(new UserType(3, "admin")),
+    DEVELOPER(new UserType(4, "developer"));
+
+    private UserType type;
+
+    public UserType getType() {
+        return type;
+    }
+
+    UserTypes(UserType type) {
+        this.type = type;
+    }
 }
